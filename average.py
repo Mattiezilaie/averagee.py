b14f4bc9068e7f3a88d3ead17244ab55cc73136a
Mattie Zilaie <your_name@domain.comAccount>
2019-10-02 17:11:49 -0700

    project 2

---
+++ average.py
@@ -0,0 +1,18 @@
+#Author: Mahtab Zilaie
+#Date: 10/2/19
+#Description: user enters five numbers and then sends back the total number
+
+print("Please enter five numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+print(num_1)
+print(num_2)
+print(num_3)
+print(num_4)
+print(num_5)
+average = (num_1 + num_2 + num_3 + num_4 + num_5)/5
+print("The average of those numbers is: ")
+print(average)
